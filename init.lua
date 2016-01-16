ed6dda78b957777e9d61d7b7f345db24d40dfa74
yumetodo <yume-wikijp@live.jp>
2016-01-16 20:33:58 +0900

    remove craft recipe comment from code

--- init.lua
+++ init.lua
@@ -73,58 +73,9 @@ minetest.register_craft({
         cooktime = 45,
 })
 
-
---[[
-
-------------------------------------------
---------       Craft Recipe       --------
-------------------------------------------
-Stick -> splitted stick x 4
-
-Cobblestone	Cobblestone	Cobblestone
-Cobblestone	Cobblestone	Cobblestone	->	Compressed Cobblestone (1)
-Cobblestone	Cobblestone	Cobblestone
-
-Compressed Cobblestone (1) -> Cobblestone x 9
-
-Compressed Cobblestone (1)	Compressed Cobblestone (1)	Compressed Cobblestone (1)
-Compressed Cobblestone (1)	splitted stick				Compressed Cobblestone (1)	->	StoneHummer
-							splitted stick
-
-Cobblestone
-StoneHummer	->	Cobblestone Dust x 4
-
-Sand	->	Sand Dust x 4
-
-group:Leaves	group:Leaves	group:Leaves
-group:Leaves					group:Leaves	->	8x Compressed Leaves
-group:Leaves	group:Leaves	group:Leaves
-
-8x Compressed Leaves	8x Compressed Leaves	8x Compressed Leaves
-8x Compressed Leaves							8x Compressed Leaves	->	64x Compressed Leaves
-8x Compressed Leaves	8x Compressed Leaves	8x Compressed Leaves
-
-64x Compressed Leaves	64x Compressed Leaves	64x Compressed Leaves
-64x Compressed Leaves							64x Compressed Leaves	->	512x Compressed Leaves
-64x Compressed Leaves	64x Compressed Leaves	64x Compressed Leaves
-
-64x Compressed Leaves	ashes				64x Compressed Leaves
-64x Compressed Leaves	Cobblestone Dust	64x Compressed Leaves	->	Dirt
-64x Compressed Leaves	ashes				64x Compressed Leaves
-
-64x Compressed Leaves	ashes		64x Compressed Leaves
-64x Compressed Leaves	Sand Dust	64x Compressed Leaves	->	Dirt
-64x Compressed Leaves	ashes		64x Compressed Leaves
-
-512x Compressed Leaves	Cobblestone Dust	512x Compressed Leaves
-512x Compressed Leaves	ashes				512x Compressed Leaves	->	Dirt x 8
-512x Compressed Leaves	Cobblestone Dust	512x Compressed Leaves
-
-512x Compressed Leaves	Sand Dust	512x Compressed Leaves
-512x Compressed Leaves	ashes		512x Compressed Leaves	->	Dirt x 9
-512x Compressed Leaves	Sand Dust	512x Compressed Leaves
-
-]]
+--
+-- Craft recipes
+--
 minetest.register_craft({
 	output = "woodtodirt:splitted_stick 4",
 	recipe = {{"moreblocks:jungle_stick"}}
