a92e275db1b82335fb78a8e627178793aaf95013
yumetodo <yume-wikijp@live.jp>
2016-01-16 15:40:11 +0900

    init commit

---
+++ comp_blocks_utility.lua
@@ -0,0 +1,158 @@
+﻿--[[
+License for code:
+  Copyright (C) 2016 yumetodo <yume-wikijp@live.jp>
+  https://github.com/Nagarei/DxLibEx/
+  Distributed under the Boost Software License, Version 1.0.
+  (See http://www.boost.org/LICENSE_1_0.txt)
+--]]
+
+--[[
+@brief is_center_nullによってdescriptionを作り分ける
+@param base_description 元の説明
+@param is_center_null "register_compblock", "register_compblock_by_group"関数に指定するis_center_null
+@param comp_level なん段階目の圧縮か
+@return 生成された名前。
+]]
+function register_compblock_impl_make_new_description(base_description, is_center_null, comp_level)
+	return is_center_null and 8^comp_level.."Compressed "..base_description or "Compressed "..base_description.." ("..i..")"
+end
+--[[
+@brief is_center_nullによってnode名を作り分ける
+@param base_name 元の名前
+@param is_center_null "register_compblock", "register_compblock_by_group"関数に指定するis_center_null
+@param comp_level なん段階目の圧縮か
+@return 生成された名前。"comp_blocks:"から始まる
+]]
+function register_compblock_impl_make_new_name(base_name, is_center_null, comp_level)
+	local re = is_center_null and "_"..8^comp_level.."x" or "_"..comp_level
+	return "comp_blocks:"..base_name..re
+end
+--[[
+@brief 圧縮・解凍レシピの登録
+@param pre_nodename 圧縮前のnode/group名
+@param new_nodename 圧縮後のnode名
+@param is_center_null "register_compblock", "register_compblock_by_group"関数に指定するis_center_null
+@param do_decompress_flag trueで解凍レシピ追加、falseで追加しない
+]]
+function register_compblock_impl_register_craft(pre_nodename, new_nodename, is_center_null, do_decompress_flag)
+	if is_center_null then
+		minetest.register_craft({
+			output = new_nodename,
+			recipe = {
+				{pre_nodename, pre_nodename, pre_nodename},
+				{pre_nodename, '', pre_nodename},
+				{pre_nodename, pre_nodename, pre_nodename}
+			}
+		})
+	else--is_center_null
+		minetest.register_craft({
+			output = new_nodename,
+			recipe = {
+				{pre_nodename, pre_nodename, pre_nodename},
+				{pre_nodename, pre_nodename, pre_nodename},
+				{pre_nodename, pre_nodename, pre_nodename}
+			}
+		})
+	end--is_center_null
+	if do_decompress_flag then
+		local decomp_re_p_num = is_center_null and 8 or 9--Cなら(is_center_null) ? 8 : 9; booleanを暗黙にarithmeticに変換できないので致し方なし
+		minetest.register_craft({
+			output = pre_nodename.." "..decomp_re_p_num,
+			recipe = {{new_nodename}}
+		})
+	end--do_decompress_flag
+end
+--[[
+@brief 圧縮部ロック系を登録する
+@param recipe 元にするブロック名。"default:acacia_tree"みたいに
+@param max_comp_level 圧縮のレベル。1以上。
+@param is_center_null 中央空白の8個圧縮のレシピにするか(true)否か(false)
+@param do_decompress_flag trueで解凍レシピ追加、falseで追加しない
+@detail
+
+
+| is_center_null | true                                   | false                              |
+|----------------|----------------------------------------|------------------------------------|
+| description    | [8^i] Compressed [元アイテムの説明]        | Compressed [元アイテムの説明]　(i)      |
+| nodename       | :comp_blocks:[元アイテムの名前]_[8^i]x     | :comp_blocks:[元アイテムの名前]_i      |
+@code
+register_compblock("default:acacia_tree", 4, false, true)
+@endcode
+]]
+function register_compblock(recipe, max_comp_level, is_center_null, do_decompress_flag)
+	local recipe_def = minetest.registered_nodes[recipe]--recipeデータの取得
+
+	if not recipe_def then
+		return
+	end
+
+	local nodename = "comp_blocks:"..recipe:split(":")[2]--"[namespace]:[node name]"から[node name]をとりだす
+	for i = 1, max_comp_level do--Cならfor(int i = 1, i < =4; ++i)
+		--
+		-- node登録
+		--
+		local def = table.copy(recipe_def)--recipeデータをコピー、書き換えていく
+		for n, tile in ipairs(def.tiles) do
+			def.tiles[n] = def.tiles[n].."^[colorize:black:"..i*45
+		end
+		def.description = register_compblock_impl_make_new_description(def.description, is_center_null, i)
+		local new_nodename = register_compblock_impl_make_new_name(group_name, is_center_null, i)
+		def.drop = new_nodename
+		minetest.register_node(":"..new_nodename, def)--いじり終わったらnode登録
+		--
+		-- Craft Recipe登録
+		--
+		--previousだから-1する.C++なら(1 == i) ? recipe : nodename + "_" + std::to_string(i - 1)
+		local previous = 1 == i and recipe or register_compblock_impl_make_new_name(group_name, is_center_null, i - 1)
+		register_compblock_impl_register_craft(previous, new_nodename, is_center_null, do_decompress_flag)
+	end
+end
+--[[
+@brief 圧縮部ロック系を登録する
+@param group_name 1段階目の圧縮に使うアイテム群であるグループ名。"leaves"のように。"group:"は不要です
+@param base_node 元にするブロック名。"default:acacia_tree"みたいに
+@param max_comp_level 圧縮のレベル。1以上。
+@param is_center_null 中央空白の8個圧縮のレシピにするか(true)否か(false)
+@param do_decompress_flag trueで解凍レシピ追加、falseで追加しない
+@detail
+
+
+| is_center_null | true                                   | false                              |
+|----------------|----------------------------------------|------------------------------------|
+| description    | [8^i] Compressed [group名]        | Compressed [group名]　(i)      |
+| nodename       | :comp_blocks:[group名]_[8^i]x     | :comp_blocks:[group名]_i      |
+@code
+register_compblock_by_group("leaves", "default:pine_needles", 4, false, true)
+@endcode
+]]
+function register_compblock_by_group(group_name, base_node, max_comp_level, is_center_null, do_decompress_flag)
+	if 0 == minetest.get_item_group(base_node, group_name) then
+		print(base_node.." is not related with `group:"..group_name.."`.")
+		return
+	end
+	local recipe_def = minetest.registered_nodes[base_node]--recipeデータの取得
+	if not recipe_def then
+		return
+	end
+
+	for i = 1 ,4 do--Cならfor(int i = 1, i < =4; ++i)
+		--
+		-- node登録
+		--
+		local def = table.copy(recipe_def)--recipeデータをコピー、書き換えていく
+		for n, tile in ipairs(def.tiles) do
+			def.tiles[n] = def.tiles[n].."^[colorize:black:"..i*45
+		end
+		def.description = register_compblock_impl_make_new_description(group_name, is_center_null, i)
+		local new_nodename = register_compblock_impl_make_new_name(group_name, is_center_null, i)
+		def.drop = new_nodename
+		def.groups = {}--comp_blocksはグループから外す
+		minetest.register_node(":"..new_nodename, def)--いじり終わったらnode登録
+		--
+		-- Craft Recipe登録
+		--
+		--previousだから-1する.C++なら(1 == i) ? "group:" + group_name : register_compblock_impl_make_new_name(group_name, is_center_null, i - 1)
+		local previous = 1 == i and "group:"..group_name or register_compblock_impl_make_new_name(group_name, is_center_null, i - 1)
+		register_compblock_impl_register_craft(previous, new_nodename, is_center_null, do_decompress_flag)
+	end
+end
---
+++ init.lua
@@ -0,0 +1,216 @@
+﻿--[[
+woodtodart
+========
+
+Copyright (c) 2015 yumetodo <yume-wikijp@live.jp>
+
+Version: 1.0
+
+Depends: charcoal
+https://forum.minetest.net/viewtopic.php?f=11&t=3971
+
+License for code:
+MIT License
+(http://opensource.org/licenses/mit-license.php)
+
+License for texture:
+Attribution 4.0 International (CC BY 4.0)
+(https://creativecommons.org/licenses/by/4.0/)
+--]]
+
+dofile(minetest.get_modpath("woodtodirt").."/comp_blocks_utility.lua")
+
+local modname = "woodtodirt"
+--
+-- New Crafting items
+--
+
+minetest.register_craftitem("woodtodart:ashes", {
+        description = "ashes",
+        inventory_image = "ash.png",
+})
+minetest.register_craftitem("woodtodart:stone_dust", {
+        description = "Stone Dust",
+        inventory_image = "stone_dust.png",
+})
+minetest.register_craftitem("woodtodart:sand_dust", {
+        description = "Stone Dust",
+        inventory_image = "sand_dust.png",
+})
+minetest.register_craftitem("woodtodart:stone_hummer", {
+        description = "Stone Hummer",
+        inventory_image = "Stone_Hummer.png",
+})
+minetest.register_craftitem("woodtodart:splitted_stick", {
+        description = "splitted stick",
+        inventory_image = "splitted_stick.png",
+})
+
+--
+-- register compblock
+--
+register_compblock("default:cobble", 4, false, true)
+register_compblock_by_group("leaves", "default:pine_needles", 3, true, false)
+
+--
+-- Cooking recipes
+--
+minetest.register_craft({
+        type = "cooking",
+        output = "woodtodart:ashes 4",
+        recipe = "charcoal:charcoal_lump",
+        cooktime = 35,
+})
+minetest.register_craft({
+        type = "cooking",
+        output = "woodtodart:ashes 4",
+        recipe = "default:coal_lump",
+        cooktime = 25,
+})
+minetest.register_craft({
+        type = "cooking",
+        output = "woodtodart:ashes",
+        recipe = "group:sapling",
+        cooktime = 45,
+})
+
+
+--[[
+
+------------------------------------------
+--------       Craft Recipe       --------
+------------------------------------------
+Stick　-> splitted stick x 4
+
+Cobblestone	Cobblestone	Cobblestone
+Cobblestone	Cobblestone	Cobblestone	->	Compressed Cobblestone (1)
+Cobblestone	Cobblestone	Cobblestone
+
+Compressed Cobblestone (1) -> Cobblestone x 9
+
+Compressed Cobblestone (1)	Compressed Cobblestone (1)	Compressed Cobblestone (1)
+Compressed Cobblestone (1)	splitted stick				Compressed Cobblestone (1)	->	StoneHummer
+							splitted stick
+
+Cobblestone
+StoneHummer	->	Cobblestone Dust x 4
+
+Sand	->	Sand Dust x 4
+
+group:Leaves	group:Leaves	group:Leaves
+group:Leaves					group:Leaves	->	8x Compressed Leaves
+group:Leaves	group:Leaves	group:Leaves
+
+8x Compressed Leaves	8x Compressed Leaves	8x Compressed Leaves
+8x Compressed Leaves							8x Compressed Leaves	->	64x Compressed Leaves
+8x Compressed Leaves	8x Compressed Leaves	8x Compressed Leaves
+
+64x Compressed Leaves	64x Compressed Leaves	64x Compressed Leaves
+64x Compressed Leaves							64x Compressed Leaves	->	512x Compressed Leaves
+64x Compressed Leaves	64x Compressed Leaves	64x Compressed Leaves
+
+64x Compressed Leaves	ashes				64x Compressed Leaves
+64x Compressed Leaves	Cobblestone Dust	64x Compressed Leaves	->	Dirt
+64x Compressed Leaves	ashes				64x Compressed Leaves
+
+64x Compressed Leaves	ashes		64x Compressed Leaves
+64x Compressed Leaves	Sand Dust	64x Compressed Leaves	->	Dirt
+64x Compressed Leaves	ashes		64x Compressed Leaves
+
+512x Compressed Leaves	Cobblestone Dust	512x Compressed Leaves
+512x Compressed Leaves	ashes				512x Compressed Leaves	->	Dirt x 8
+512x Compressed Leaves	Cobblestone Dust	512x Compressed Leaves
+
+512x Compressed Leaves	Sand Dust	512x Compressed Leaves
+512x Compressed Leaves	ashes		512x Compressed Leaves	->	Dirt x 9
+512x Compressed Leaves	Sand Dust	512x Compressed Leaves
+
+:comp_blocks:[元アイテムの名前]_[8^i]x
+comp_blocks:leaves_64x
+comp_blocks:leaves_512x
+default:cobble
+default:stick
+default:sand
+default:dirt
+moreblocks:cobble_compressed
+comp_blocks:cobble_1
+default:pine_needles
+]]
+minetest.register_craft({
+	output = "woodtodart:splitted_stick　4",
+	recipe = {{"default:stick"}}
+})
+minetest.register_craft({
+	output = "woodtodart:stone_hummer",
+	recipe = {
+	{"comp_blocks:cobble_1", "comp_blocks:cobble_1", "comp_blocks:cobble_1"},
+	{"comp_blocks:cobble_1", "woodtodart:splitted_stick", "comp_blocks:cobble_1"},
+	{"", "woodtodart:splitted_stick", ""},
+	},
+})
+minetest.register_craft({
+	output = "woodtodart:stone_dust 4",
+	recipe = {
+	{"default:cobble"},
+	{"woodtodart:stone_hummer"},
+	},
+})
+minetest.register_craft({
+	output = "woodtodart:sand_dust　4",
+	recipe = {{"default:sand"}}
+})
+minetest.register_craft({
+	output = "default:dirt",
+	recipe = {
+	{"comp_blocks:leaves_64x", "woodtodart:ashes", "comp_blocks:leaves_64x"},
+	{"comp_blocks:leaves_64x", "woodtodart:stone_dust", "comp_blocks:leaves_64x"},
+	{"comp_blocks:leaves_64x", "woodtodart:ashes", "comp_blocks:leaves_64x"},
+	},
+})
+minetest.register_craft({
+	output = "default:dirt",
+	recipe = {
+	{"comp_blocks:leaves_64x", "woodtodart:ashes", "comp_blocks:leaves_64x"},
+	{"comp_blocks:leaves_64x", "woodtodart:sand_dust", "comp_blocks:leaves_64x"},
+	{"comp_blocks:leaves_64x", "woodtodart:ashes", "comp_blocks:leaves_64x"},
+	},
+})
+minetest.register_craft({
+	output = "default:dirt 8",
+	recipe = {
+	{"comp_blocks:leaves_512x", "woodtodart:stone_dust", "comp_blocks:leaves_512x"},
+	{"comp_blocks:leaves_512x", "woodtodart:ashes", "comp_blocks:leaves_512x"},
+	{"comp_blocks:leaves_512x", "woodtodart:stone_dust", "comp_blocks:leaves_512x"},
+	},
+})
+minetest.register_craft({
+	output = "default:dirt 9",
+	recipe = {
+	{"comp_blocks:leaves_512x", "woodtodart:sand_dust", "comp_blocks:leaves_512x"},
+	{"comp_blocks:leaves_512x", "woodtodart:ashes", "comp_blocks:leaves_512x"},
+	{"comp_blocks:leaves_512x", "woodtodart:sand_dust", "comp_blocks:leaves_512x"},
+	},
+})
+
+if minetest.get_modpath("moreblocks") ~= nil then--moreblocks has Compressed Cobblestone
+	minetest.register_craft({
+		output = "comp_blocks:cobble_2",
+		recipe = {
+		{"moreblocks:cobble_compressed", "moreblocks:cobble_compressed", "moreblocks:cobble_compressed"},
+		{"moreblocks:cobble_compressed", "moreblocks:cobble_compressed", "moreblocks:cobble_compressed"},
+		{"moreblocks:cobble_compressed", "moreblocks:cobble_compressed", "moreblocks:cobble_compressed"},
+		},
+	})
+	minetest.register_craft({
+		output = "woodtodart:sand_dust　4",
+		recipe = {{"default:sand"}}
+	})
+	minetest.register_craft({
+		output = "woodtodart:stone_hummer",
+		recipe = {
+		{"moreblocks:cobble_compressed", "moreblocks:cobble_compressed", "moreblocks:cobble_compressed"},
+		{"moreblocks:cobble_compressed", "woodtodart:splitted_stick", "moreblocks:cobble_compressed"},
+		{"", "woodtodart:splitted_stick", ""},
+		},
+	})
+end
