df8f781acd31620e29f69cf3f0d0238519a1c40f
yumetodo <yume-wikijp@live.jp>
2016-01-17 18:09:17 +0900

    #4 木炭の追加 棒をdefault:stickではなくgroup:stickでのレシピに

--- init.lua
+++ init.lua
@@ -34,6 +34,18 @@ minetest.register_craftitem("woodtodirt:sand_dust", {
         description = "Stone Dust",
         inventory_image = "woodtodirt_sand_dust.png",
 })
+minetest.register_craftitem("woodtodirt:splitted_stick", {
+        description = "splitted stick",
+        inventory_image = "woodtodirt_splitted_stick.png",
+})
+minetest.register_craftitem("woodtodirt:charcoal", {
+        description = "Charcoal",
+        inventory_image = "woodtodirt_charcoal.png",
+})
+
+--
+-- New Crafting tools
+--
 minetest.register_tool("woodtodirt:stone_hammer", {
         description = "Stone Hummer",
         inventory_image = "woodtodirt_stone_hammer.png",
@@ -41,10 +53,7 @@ minetest.register_tool("woodtodirt:stone_hammer", {
             return
         end,
 })
-minetest.register_craftitem("woodtodirt:splitted_stick", {
-        description = "splitted stick",
-        inventory_image = "woodtodirt_splitted_stick.png",
-})
+
 --[[
 2016-01-16 21:10:58: ACTION[ServerThread]: player singleplayer crafts comp_blocks:leaves_64x
 ]]
@@ -62,32 +71,44 @@ if minetest.get_modpath("charcoal") then
 	        type = "cooking",
 	        output = "woodtodirt:ashes 4",
 	        recipe = "charcoal:charcoal_lump",
-	        cooktime = 25,
+	        cooktime = 10,
 	})
 end
 minetest.register_craft({
         type = "cooking",
+        output = "woodtodirt:charcoal",
+        recipe = "group:tree",
+        cooktime = 38,
+})
+minetest.register_craft({
+        type = "cooking",
+        output = "woodtodirt:ashes 4",
+        recipe = "woodtodirt:charcoal",
+        cooktime = 10,
+})
+minetest.register_craft({
+        type = "cooking",
         output = "woodtodirt:ashes 4",
         recipe = "default:coal_lump",
-        cooktime = 15,
+        cooktime = 10,
 })
 minetest.register_craft({
         type = "cooking",
         output = "woodtodirt:ashes",
         recipe = "group:sapling",
-        cooktime = 30,
+        cooktime = 25,
 })
 minetest.register_craft({
         type = "cooking",
         output = "woodtodirt:ashes",
         recipe = "comp_blocks:leaves_512x",
-        cooktime = 17,
+        cooktime = 13,
 })
 minetest.register_craft({
         type = "cooking",
         output = "woodtodirt:ashes 8",
         recipe = "comp_blocks:leaves_4096x",
-        cooktime = 18,
+        cooktime = 15,
 })
 --
 -- fuel recipes
@@ -117,17 +138,32 @@ minetest.register_craft({
 		recipe = "comp_blocks:leaves_4096x",
 		burntime = 128
 })
+minetest.register_craft({
+        type = "fuel",
+        recipe = "woodtodirt:charcoal",
+        burntime = 40,
+})
 
 --
 -- Craft recipes
 --
 minetest.register_craft({
-	output = "woodtodirt:splitted_stick 4",
-	recipe = {{"moreblocks:jungle_stick"}}
+    output = 'default:torch 4',
+    recipe = {
+	{'woodtodirt:charcoal'},
+    {'group:stick'},
+    }
+})
+minetest.register_craft({
+    output = 'default:torch',
+    recipe = {
+	{'woodtodirt:ashes'},
+    {'woodtodirt:splitted_stick'},
+    }
 })
 minetest.register_craft({
 	output = "woodtodirt:splitted_stick 4",
-	recipe = {{"default:stick"}}
+	recipe = {{"group:stick"}}
 })
 minetest.register_craft({
 	output = "woodtodirt:stone_hammer",
@@ -135,6 +171,14 @@ minetest.register_craft({
 	{"comp_blocks:cobble_1", "comp_blocks:cobble_1", "comp_blocks:cobble_1"},
 	{"comp_blocks:cobble_1", "woodtodirt:splitted_stick", "comp_blocks:cobble_1"},
 	{"", "woodtodirt:splitted_stick", ""},
+	},
+})
+minetest.register_craft({
+	output = "woodtodirt:stone_hammer",
+	recipe = {
+	{"comp_blocks:cobble_1", "comp_blocks:cobble_1", ""},
+	{"comp_blocks:cobble_1", "comp_blocks:cobble_1", "woodtodirt:splitted_stick"},
+	{"comp_blocks:cobble_1", "comp_blocks:cobble_1", ""},
 	},
 })
 minetest.register_craft({
@@ -202,6 +246,24 @@ if minetest.get_modpath("moreblocks") then--moreblocks has Compressed Cobbleston
 		{"", "woodtodirt:splitted_stick", ""},
 		},
 	})
+	minetest.register_craft({
+		output = "woodtodirt:stone_hammer",
+		recipe = {
+		{"moreblocks:cobble_compressed", "moreblocks:cobble_compressed", ""},
+		{"moreblocks:cobble_compressed", "moreblocks:cobble_compressed", "woodtodirt:splitted_stick"},
+		{"moreblocks:cobble_compressed", "moreblocks:cobble_compressed", ""},
+		},
+	})
+end
+if minetest.get_modpath("charcoal") then
+	minetest.register_craft({
+		output = "woodtodirt:charcoal",
+		recipe = {{"charcoal:charcoal_lump"}}
+	})
+	minetest.register_craft({
+		output = "charcoal:charcoal_lump",
+		recipe = {{"woodtodirt:charcoal"}}
+	})
 end
 
 --
@@ -211,7 +273,7 @@ minetest.register_on_craft(function(_, _, old_craft_grid, craft_inv)
 	--材料からハンマーを探す
 	for i, item in ipairs(old_craft_grid) do
 		if item:get_name() == "woodtodirt:stone_hammer" then
-			item:add_wear(65535 / 2)--ハンマーに5割のダメージを追加する
+			item:add_wear(65535 / 2 + 1)--ハンマーに5割のダメージを追加する
 			craft_inv:set_stack("craft", i, item)
 		end
 	end
